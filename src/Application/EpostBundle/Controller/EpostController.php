1e7087e49024a208d6caa803cfef4127c7c01615
manuroot <mroot72000@yahoo.fr>
2013-05-26 11:24:27 +0200

    lexikform sur all news

--- src/Application/EpostBundle/Controller/EpostController.php
+++ src/Application/EpostBundle/Controller/EpostController.php
@@ -19,6 +19,8 @@ use Symfony\Component\Security\Acl\Domain\UserSecurityIdentity;
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Application\EpostBundle\Form\EpostTypeFiltres;
+
 
 /**
  * Epost controller.
@@ -88,8 +90,13 @@ class EpostController extends Controller {
      *  CREATION DU PAGINATOR
      * 
       =================================================================== */
-
-    //$defaut_paginator=array('pagename'=>'page1','sort'=>'sort1','sortfield'=>'sort1');
+ /*$paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+                $query, $this->get('request')->query->get('page', 1), 10
+        );
+        $pagination->setTemplate('ApplicationCertificatsBundle:pagination:sliding.html.twig');
+       
+    //$defaut_paginator=array('pagename'=>'page1','sort'=>'sort1','sortfield'=>'sort1');*/
     private function createpaginator($query, $num_perpage = 5, $defaut_paginator = null) {
 
         $paginator = $this->get('knp_paginator');
@@ -112,7 +119,8 @@ class EpostController extends Controller {
             "sortDirectionParameterName" => $sortDirectionParameterName,
             'sortFieldParameterName' => $sortFieldParameterName)
         );
-
+     //  $pagination->setTemplate('ApplicationEpostBundle:pagination:sliding.html.twig');
+             
         $pagination->setTemplate('ApplicationEpostBundle:pagination:twitter_bootstrap_pagination.html.twig');
         return $pagination;
     }
@@ -241,13 +249,59 @@ class EpostController extends Controller {
         $em = $this->getDoctrine()->getManager();
         $session = $this->getRequest()->getSession();
         $session->set('buttonretour', 'epost_indexadmin');
+     
+          $searchForm = $this->createForm(new EpostTypeFiltres());
+          if ($this->get('request')->query->has('submit-filter')) {
+             // echo "submit filters";exit(1);
+            // bind values from the request
+                 $searchForm->bind($this->get('request'));
+          //var_dump($searchForm);
+         //   $searchForm->bind($this->get('request')->get('submit-filter'));
+          //  $query = $em->getRepository('ApplicationEpostBundle:Epost')->getMyPager(array());
+               //$querya = $em->getRepository('ApplicationEpostBundle:Epost')->getMyPager(array());
+           $filterBuilder = $this->get('doctrine.orm.entity_manager')
+                    ->getRepository('ApplicationEpostBundle:Epost')
+                   // ->createQueryBuilder('e');
+          ->createQueryBuilder('a')
+                      ->select('a,c,e,d,f,b')
+                     ->leftJoin('a.categorie', 'c')
+                     ->leftJoin('a.globalnote', 'e')
+                    ->leftJoin('a.idStatus', 'd')
+                    ->leftJoin('a.imageMedia', 'f')
+                     ->leftJoin('a.proprietaire', 'b')
+                   
+                //   ->leftJoin('a.comments', 'u')
+              /*  ->leftJoin('a.globalnote', 'e')
+                ->leftJoin('a.imageMedia', 'f')
+                 ->leftJoin('a.comments', 'u')
+               */
+                 //   ->select('a,c,d,e,f,u')
+            /*   ->select('a,b,c,d,e,f,u')
+                 ->leftJoin('a.proprietaire', 'b')
+               
+                ->leftJoin('a.idStatus', 'd')
+                ->leftJoin('a.globalnote', 'e')
+                ->leftJoin('a.imageMedia', 'f')
+                 ->leftJoin('a.comments', 'u')*/
+                   ->add('orderBy', 'a.id DESC')
+                ;
+                 $query = $this->get('lexik_form_filter.query_builder_updater')->addFilterConditions($searchForm, $filterBuilder)->getQuery();
+      
+            //     var_dump($query->getDql());exit(1);
+                   
+                   } else {
+           // $em = $this->getDoctrine()->getManager();
+           $query = $em->getRepository('ApplicationEpostBundle:Epost')->getMyPager(array());
+        }
+         
 
-
-        $query = $em->getRepository('ApplicationEpostBundle:Epost')->getMyPager(array());
-        //$query = $em->getRepository('ApplicationEpostBundle:Epost')->myFind();
-        $paginationa = $this->createpaginator($query, 10);
-        return $this->render('ApplicationEpostBundle:Epost:indexadmin.html.twig', array(
+ 
+       
+    //$defaut_paginator=array('pagename'=>'page1','sort'=>'sort1','sortfield'=>'sort1');
+        $paginationa = $this->createpaginator($query, 5);
+        return $this->render('ApplicationEpostBundle:Epost:indexall.html.twig', array(
                     'paginationa' => $paginationa,
+                'search_form' => $searchForm->createView(),
                 ));
     }
 
---
+++ src/Application/EpostBundle/Form/EpostTypeFiltres.php
@@ -0,0 +1,109 @@
+<?php
+
+/*
+ * This file is part of the Sonata package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Application\EpostBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Lexik\Bundle\FormFilterBundle\Filter\Extension\Type\TextFilterType as TextFilterType;
+use Lexik\Bundle\FormFilterBundle\Filter\FilterOperands;
+
+use Doctrine\ORM\EntityRepository;
+
+
+class EpostTypeFiltres extends AbstractType {
+
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+
+
+        $builder
+     
+                ->add('name', 'filter_text', array(
+                    'condition_pattern' => FilterOperands::OPERAND_SELECTOR,
+                    'widget_addon' => array(
+                        'icon' => 'user',
+                        'type' => 'prepend'
+                    ),))
+               
+   ->add('categorie', 'filter_entity', array(
+                'class' => 'Application\EpostBundle\Entity\EpostCategories',
+                'property' => 'name'
+            ))
+    
+                ->add('createdAt', 'filter_date_range', array(
+                     'label' => 'Date de Fin',
+                    'left_date' => array(
+                        'widget' => 'single_text'
+                    /* 'time_widget' => 'single_text' */
+                    ),
+                    'right_date' => array(
+                        'widget' => 'single_text'
+                    /* 'time_widget' => 'single_text' */
+                    ),
+                ))
+                 ->add('updatedAt', 'filter_date_range', array(
+                     'label' => 'Date de Fin',
+                    'left_date' => array(
+                        'widget' => 'single_text'
+                    /* 'time_widget' => 'single_text' */
+                    ),
+                    'right_date' => array(
+                        'widget' => 'single_text'
+                    /* 'time_widget' => 'single_text' */
+                    ),
+                ))
+          /*  ->add('proprietaire', 'entity', array(
+           'class' => 'Application\Sonata\UserBundle\Entity\User',
+           'query_builder' => function(EntityRepository $em) {
+                return $em->createQueryBuilder('u')
+                                ->orderBy('u.username', 'ASC');
+            },
+             'property' => 'username',
+             'multiple' => false,
+            'required' => true,
+            'label' => 'Proprietaire',
+            'empty_value' => '--- Choisir une option ---'
+        ))*/
+              ->add('proprietaire', 'filter_entity', array(
+                'class' => 'Application\Sonata\UserBundle\Entity\User',
+                'property' => 'username'
+            ));
+        ;
+    }
+
+   
+
+    public function getName() {
+        return 'epost_filter';
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+            'csrf_protection' => false,
+            'validation_groups' => array('filtering') // avoid NotBlank() constraint-related message
+        ));
+    }
+
+    /*
+      public function getDefaultOptions(array $options)
+      {
+      return array(
+      'validation_groups' => array('no_validation')
+      );
+      }
+
+      public function getName()
+      {
+      return 'team_filter';
+      } */
+}

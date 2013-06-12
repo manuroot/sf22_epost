f20b9216c96c51c4e1e31e13308694993321b790
manuroot <mroot72000@yahoo.fr>
2013-06-12 19:58:58 +0200

    maj graphs

--- src/Application/EpostBundle/Entity/Epost.php
+++ src/Application/EpostBundle/Entity/Epost.php
@@ -390,6 +390,7 @@ class Epost {
         $this->setUpdatedAt(new \DateTime());
        // $this->setUpdatedAt(new \DateTime());
         $this->setSlug($this->getName());
+      //  echo "TEST=" .  $this->xslugify('é'); // --> "e"
         // reclaculer la note globale ??
     }
 
@@ -718,15 +719,20 @@ class Epost {
 
     public function slugify($text) {
         // replace non letter or digits by -
+         $text=$this->createSlug($text); 
+
         $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
 
         // trim
-        $text = trim($text, '-');
+        $text = trim($text, '--');
 
         // transliterate
         if (function_exists('iconv')) {
             $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+         // echo "pas de fonction iconv";  exit(1);
         }
+        
+     
 
         // lowercase
         $text = strtolower($text);
@@ -854,4 +860,56 @@ class Epost {
         foreach($this->children as $child)
             $child->setParent(null);
     }*/
+    
+    public function xslugify($text)
+{
+  // replace non letter or digits by -
+  $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+  // trim
+  $text = trim($text, '-');
+
+  // transliterate
+  if (function_exists('iconv'))
+  {
+    $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+  }
+
+  // lowercase
+  $text = strtolower($text);
+
+  // remove unwanted characters
+  $text = preg_replace('#[^-\w]+#', '', $text);
+
+  if (empty($text))
+  {
+    return 'n-a';
+  }
+
+  return $text;
 }
+
+
+public function createSlug($string) {
+
+    $table = array(
+            'Š'=>'S', 'š'=>'s', 'Đ'=>'Dj', 'đ'=>'dj', 'Ž'=>'Z', 'ž'=>'z', 'Č'=>'C', 'č'=>'c', 'Ć'=>'C', 'ć'=>'c',
+            'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+            'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O',
+            'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss',
+            'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e',
+            'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o',
+            'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b',
+            'ÿ'=>'y', 'Ŕ'=>'R', 'ŕ'=>'r', '/' => '-', ' ' => '-'
+    );
+
+    // -- Remove duplicated spaces
+   // $stripped = preg_replace(array('/\s{2,}/', '/[\t\n]/'), ' ', $string);
+
+    // -- Returns the slug
+    return strtolower(strtr($string, $table));
+
+
+}
+
+  }
